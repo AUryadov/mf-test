6634bcd5f36a07b14e7e414d9445c120080b77c5
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-03 13:25:08 +0400

    fix

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-export const mount = el => {
-    ReactDOM.render(
-        <App />,
-        el
-    );
+export const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    const { pathname } = history;
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        },
+    };
 };
 
-if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+if (process.env.NODE_ENV === "development") {
+    const devRoot = document.querySelector("#_marketing-dev-root");
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
